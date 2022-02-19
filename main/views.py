b61fea0f555aabb6db6232f086b5f093039cff69
IstAviX <istavix@gmail.com>
2022-02-19 12:22:01 +0100

    Code clean-up

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-
 from time import strftime
 from xmlrpc.client import DateTime
 from django.shortcuts import render
@@ -7,7 +6,7 @@ from django.shortcuts import redirect
 from .forms import MileageExpenseForm, RefundRequestForm, ExpenseReportForm, AdvanceForm
 from .forms import RefundRequestForm
 from .models import ExpenseReport, Collaborator, ExpenseLine, Advance, Mission, RefundRequest, MileageExpense
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
@@ -19,36 +18,34 @@ import locale
 import mimetypes
 
 
-
-
-
-
 def logoutPage(request):
     logout(request)
-    return render(request,'main/logout.html')
+    return render(request, 'main/logout.html')
+
 
 def user_login(request):
-    #handle rediredct
+    # handle rediredct
     next = request.GET.get('next', '/')
-    
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username =username, password = password)
- 
+        user = authenticate(request, username=username, password=password)
+
         if user is not None:
-            login(request,user)
-            #if the user is valid and comes from another page, redirect him
-            if(next != '/'):
+            login(request, user)
+            # if the user is valid and comes from another page, redirect him
+            if next != '/':
                 return redirect(next)
             return redirect('/void')
         else:
             form = AuthenticationForm()
-            return render(request,'main/login.html',{'form':form})
-     
+            return render(request, 'main/login.html', {'form': form})
+
     else:
         form = AuthenticationForm()
-        return render(request, 'main/login.html', {'form':form})
+        return render(request, 'main/login.html', {'form': form})
+
 
 def void(request):
     return render(request, 'main/void.html')
@@ -59,43 +56,47 @@ def save_file(f):
         for chunk in f.chunks():
             destination.write(chunk)
 
+
 @login_required(login_url='/login/')
 def sHistoric(request):
-	u = Collaborator.objects.get(user = request.user)
-	service = u.departmentHead
-	if service is not None:
-		"""uL = Collaborator.objects.filter(service=service)
-		for user in uL:"""
-		print("t")
+    u = Collaborator.objects.get(user=request.user)
+    service = u.departmentHead
+    if service is not None:
+        """uL = Collaborator.objects.filter(service=service)
+        for user in uL:"""
+        print("t")
 
-    return render(request,'main/historic.html')
+    return render(request, 'main/historic.html')
+
 
 @login_required(login_url='/login/')
 def cHistoric(request):
-    u = Collaborator.objects.get(user = request.user)
+    u = Collaborator.objects.get(user=request.user)
 
     expRepL = []
     expLinDict = {}
     advDict = {}
     mileDict = {}
     missionDict = {}
-    tempELDict ={}
+    tempELDict = {}
     tempMDict = {}
     if ExpenseReport.objects.filter(collaborator=u).count() >= 1:
-        expRepL = list(ExpenseReport.objects.filter(collaborator = u).order_by('year', '-month'))
+        expRepL = list(ExpenseReport.objects.filter(collaborator=u).order_by('year', '-month'))
         for expRep in expRepL:
-            filt = list(RefundRequest.objects.filter(expenseReport = expRep))
+            filt = list(RefundRequest.objects.filter(expenseReport=expRep))
             filtMiss = [f.mission for f in filt]
-            filtMiss = list(set(filtMiss))#remove duplicates
+            filtMiss = list(set(filtMiss))  # remove duplicates
             missionDict[expRep] = filtMiss
             for miss in filtMiss:
-                expLinDict[(expRep, miss)] = list(RefundRequest.objects.filter(expenseReport = expRep, mission = miss))
-                if MileageExpense.objects.filter(expenseReport = expRep, mission = miss).count() >= 1:
-                    tempMDict[miss] = list(MileageExpense.objects.filter(expenseReport = expRep, mission = miss))
+                expLinDict[(expRep, miss)] = list(RefundRequest.objects.filter(expenseReport=expRep, mission=miss))
+                if MileageExpense.objects.filter(expenseReport=expRep, mission=miss).count() >= 1:
+                    tempMDict[miss] = list(MileageExpense.objects.filter(expenseReport=expRep, mission=miss))
                     mileDict[expRep] = tempMDict
 
-    context = {'expRepL' : expRepL, 'collab' : u, 'expLinDict' : expLinDict, 'missDict' : missionDict, 'mileDict' : mileDict, 'advDict' : advDict}
-    return render(request,'main/clientHistoric.html',context)
+    context = {'expRepL': expRepL, 'collab': u, 'expLinDict': expLinDict, 'missDict': missionDict, 'mileDict': mileDict,
+               'advDict': advDict}
+    return render(request, 'main/clientHistoric.html', context)
+
 
 ################################################################
 #                 validator Homepage                           #
@@ -103,60 +104,52 @@ def cHistoric(request):
 
 @login_required(login_url='/login/')
 def valid(request):
-    validor = Collaborator.objects.get(user = request.user) # valideur
-    CollaboratorList = [] # liste des collaborateurs du valideur
-    DictNoteDeFrais = {} # dict de [collaborateur : [liste de notes de frais] ]
-    DictLigneDeFrais= {} # dict de [Note de frais: [liste de ExpenseLine ] ]
-    DictMission = {} # dict de  [Note de frais : [mission] ]
+    validor = Collaborator.objects.get(user=request.user)  # valideur
+    CollaboratorList = []  # liste des collaborateurs du valideur
+    DictNoteDeFrais = {}  # dict de [collaborateur : [liste de notes de frais] ]
+    DictLigneDeFrais = {}  # dict de [Note de frais: [liste de ExpenseLine ] ]
+    DictMission = {}  # dict de  [Note de frais : [mission] ]
 
-    if Collaborator.objects.filter(validator= validor).count() >= 1: # on ne fait rien si personne n'a ce valideur
-        CollaboratorList = list(Collaborator.objects.filter(validator= validor))
+    if Collaborator.objects.filter(validator=validor).count() >= 1:  # on ne fait rien si personne n'a ce valideur
+        CollaboratorList = list(Collaborator.objects.filter(validator=validor))
 
         # recuperation de ses notes de frais, peut etre mettre une date limite sinon tout sera envoye
         for collabo in CollaboratorList:
-            if ExpenseReport.objects.filter(collaborator = collabo).count() >= 1: # on ne fait rien si pas de note de frais
-                DictNoteDeFrais[collabo]=list(ExpenseReport.objects.filter(collaborator = collabo))
+            if ExpenseReport.objects.filter(
+                    collaborator=collabo).count() >= 1:  # on ne fait rien si pas de note de frais
+                DictNoteDeFrais[collabo] = list(ExpenseReport.objects.filter(collaborator=collabo))
 
-                
                 for notedefraise in DictNoteDeFrais[collabo]:
-                    DictMission[notedefraise]=[]
-                    if(RefundRequest.objects.filter(expenseReport = notedefraise).count() >= 1):
-                        filt = list(RefundRequest.objects.filter(expenseReport = notedefraise))
-                        Mission=[f.mission for f in filt]
-                        DictMission[notedefraise]+=Mission
-                    
-                    if(Advance.objects.filter(expenseReport = notedefraise).count() >= 1):
-                        filt = list(Advance.objects.filter(expenseReport = notedefraise))
-                        Mission=[f.mission for f in filt]
-                        DictMission[notedefraise]+=Mission
+                    DictMission[notedefraise] = []
+                    if RefundRequest.objects.filter(expenseReport=notedefraise).count() >= 1:
+                        filt = list(RefundRequest.objects.filter(expenseReport=notedefraise))
+                        Mission = [f.mission for f in filt]
+                        DictMission[notedefraise] += Mission
 
-                    if(MileageExpense.objects.filter(expenseReport = notedefraise).count() >= 1):
-                        filt = list(MileageExpense.objects.filter(expenseReport = notedefraise))
-                        Mission=[f.mission for f in filt]
-                        DictMission[notedefraise]+=Mission
-                        
-                
-                    
+                    if Advance.objects.filter(expenseReport=notedefraise).count() >= 1:
+                        filt = list(Advance.objects.filter(expenseReport=notedefraise))
+                        Mission = [f.mission for f in filt]
+                        DictMission[notedefraise] += Mission
+
+                    if MileageExpense.objects.filter(expenseReport=notedefraise).count() >= 1:
+                        filt = list(MileageExpense.objects.filter(expenseReport=notedefraise))
+                        Mission = [f.mission for f in filt]
+                        DictMission[notedefraise] += Mission
 
             # on associe a chaque note de frais envoyee les lignes correspondantes
             for note in DictNoteDeFrais[collabo]:
-                DictLigneDeFrais[note]=[] 
+                DictLigneDeFrais[note] = []
                 # ajout de ses advances 
-                DictLigneDeFrais[note]+=list(Advance.objects.filter(expenseReport= note).filter(state = "sent"))
+                DictLigneDeFrais[note] += list(Advance.objects.filter(expenseReport=note).filter(state="sent"))
                 # ajout de ses lignes de frais
-                DictLigneDeFrais[note]+=list(RefundRequest.objects.filter(expenseReport= note).filter(state = "sent"))
+                DictLigneDeFrais[note] += list(RefundRequest.objects.filter(expenseReport=note).filter(state="sent"))
                 # ajout de ses frais kilometriques
-                DictLigneDeFrais[note]+=list(MileageExpense.objects.filter(expenseReport= note).filter(state = "sent"))
-        
+                DictLigneDeFrais[note] += list(MileageExpense.objects.filter(expenseReport=note).filter(state="sent"))
 
-    context = {'CollaboratorList' : CollaboratorList, 'DictNoteDeFrais' : DictNoteDeFrais, 'DictLigneDeFrais' : DictLigneDeFrais, 'validor' : validor,'DictMission' : DictMission }
-    return render(request,'main/valid.html',context)
+    context = {'CollaboratorList': CollaboratorList, 'DictNoteDeFrais': DictNoteDeFrais,
+               'DictLigneDeFrais': DictLigneDeFrais, 'validor': validor, 'DictMission': DictMission}
+    return render(request, 'main/valid.html', context)
 
-
-
-
-    
-    
     # missionDict = {}
     # tempDict ={}
 
@@ -191,13 +184,13 @@ def download_file(request, filename=''):
         return response
     else:
         # Load the template
-        return render(request, 'void.html')
-
+        return render(request, 'main/void.html')
 
 
 def collabAndReport(request):
-    if (locale.LC_TIME != 'fr'):  # avoid our month to be in english. As the database is in french, we force the format to be in french
-            locale.setlocale(locale.LC_TIME, 'fr')
+    if (
+            locale.LC_TIME != 'fr'):  # avoid our month to be in english. As the database is in French, we force the format to be in French
+        locale.setlocale(locale.LC_TIME, 'fr')
     currMonth = datetime.datetime.now()
     currYear = currMonth.strftime("%Y")
     currMonth = currMonth.strftime("%B")
@@ -208,37 +201,38 @@ def collabAndReport(request):
 
     col = Collaborator.objects.get(user=request.user)
     try:
-        currExpenseReport = ExpenseReport.objects.get(collaborator=col, month=currMonth, year = currYear )
+        currExpenseReport = ExpenseReport.objects.get(collaborator=col, month=currMonth, year=currYear)
     except ExpenseReport.DoesNotExist:
         newReport = ExpenseReport(collaborator=col, month=currMonth, year=currYear)
         newReport.save()
         currExpenseReport = newReport
 
-    return col,currExpenseReport
+    return col, currExpenseReport
 
 
 ################################################################
 #                      RefundRequest                           #
 ################################################################
 @login_required(login_url='/login/')
-def createRefundRequest(request, RefReq = None):
-    #default the date to today
+def createRefundRequest(request, RefReq=None):
+    # default the date to today
     today = datetime.date.today()
     today = today.strftime("%d/%m/%Y")
     # Handle the users that does not have a report yet
-    col,currExpenseReport = collabAndReport(request)
+    col, currExpenseReport = collabAndReport(request)
 
-    #if we give a refundrequest, display its state in the form
-    form = RefundRequestForm(initial={'date':today}, collab=col, req = request)
-    #if we give a refund request, and this refund request is not accepted (redundant with a verification in the html)
+    # if we give a refundrequest, display its state in the form
+    form = RefundRequestForm(initial={'date': today}, collab=col, req=request)
+    # if we give a refund request, and this refund request is not accepted (redundant with a verification in the html)
     if RefReq is not None and RefReq.state != RefundRequest.accepted:
         col = RefReq.expenseReport.collaborator
-        form = RefundRequestForm(instance = RefReq, collab=col, req=request)#we pass the instance of the already existing refund request to let the model form generate itself
+        form = RefundRequestForm(instance=RefReq, collab=col,
+                                 req=request)  # we pass the instance of the already existing refund request to let the model form generate itself
 
     if request.method == 'POST':
-        form = RefundRequestForm(request.POST, request.FILES,instance = RefReq, collab=col, req = request)
+        form = RefundRequestForm(request.POST, request.FILES, instance=RefReq, collab=col, req=request)
         if form.is_valid():
-            if RefReq is None:#if the refund request is not given, we check wich button is pressed
+            if RefReq is None:  # if the refund request is not given, we check wich button is pressed
                 toValidate = RefundRequest.draft
                 if 'Submit' in request.POST:
                     toValidate = RefundRequest.sent
@@ -251,11 +245,11 @@ def createRefundRequest(request, RefReq = None):
                 save_file(request.FILES['proof'])
                 return redirect('/void')
             else:
-                obj = form.save(commit = False)
+                obj = form.save(commit=False)
                 obj.collaborator = col
                 obj.validator = col.validator
                 obj.state = RefundRequest.sent
-                if obj.proof != RefReq.proof :#if we give a new proof, we delete the old one from the server
+                if obj.proof != RefReq.proof:  # if we give a new proof, we delete the old one from the server
                     RefReq.dele()
                     save_file(request.FILES['proof'])
                 else:
@@ -264,7 +258,7 @@ def createRefundRequest(request, RefReq = None):
                 obj.save()
                 return redirect('/void')
         else:
-            print("ERROR : ",form.errors)
+            print("ERROR : ", form.errors)
     context = {'form': form}
     return render(request, 'main/form.html', context)
 
@@ -276,10 +270,10 @@ def createRefundRequest(request, RefReq = None):
 @login_required(login_url='/login/')
 def createAdvanceRequest(request):
     col, expRep = collabAndReport(request)
-    #default the date to today
+    # default the date to today
     today = datetime.date.today()
     today = today.strftime("%d/%m/%Y")
-    form = AdvanceForm(collab=col, initial={'date':today})
+    form = AdvanceForm(collab=col, initial={'date': today})
     if request.method == 'POST':
 
         form = AdvanceForm(request.POST, request.FILES, collab=col)
@@ -304,7 +298,6 @@ def createAdvanceRequest(request):
     return render(request, 'main/form.html', context)
 
 
-
 ################################################################
 #                         Mileage                              #
 ################################################################
@@ -312,10 +305,10 @@ def createAdvanceRequest(request):
 @login_required(login_url='/login/')
 def createMileageExpense(request):
     col, expRep = collabAndReport(request)
-    #default the date to today
+    # default the date to today
     today = datetime.date.today()
     today = today.strftime("%d/%m/%Y")
-    form = MileageExpenseForm(collab=col, initial={'date':today})
+    form = MileageExpenseForm(collab=col, initial={'date': today})
     if request.method == 'POST':
 
         form = MileageExpenseForm(request.POST, request.FILES, collab=col)
@@ -340,35 +333,13 @@ def createMileageExpense(request):
     context = {'form': form}
     return render(request, 'main/form.html', context)
 
-def download_file(request, filename=''):
-    if filename != '':
-        # Define Django project base directory
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        # Define the full file path
-        filepath = BASE_DIR + '/proofs/' + filename
-        # Open the file for reading content
-        path = open(filepath, 'rb')
-        # Set the mime type
-        mime_type, _ = mimetypes.guess_type(filepath)
-        # Set the return value of the HttpResponse
-        response = HttpResponse(path, content_type=mime_type)
-        # Set the HTTP header for sending to browser
-        response['Content-Disposition'] = "attachment; filename=%s" % filename
-        # Return the response value
-        return response
-    else:
-        # Load the template
-        return render(request, 'main/void.html')
-
-
-
 
 ################################################################
 #                     Modify Refund Request                    #
 ################################################################
 @login_required(login_url='/login/')
 def modifyRefund(request, refId):
-    RefReq = RefundRequest.objects.get(id = refId)
+    RefReq = RefundRequest.objects.get(id=refId)
     print(RefReq)
     return createRefundRequest(request, RefReq=RefReq)
 
@@ -384,7 +355,6 @@ def createExpenseReport(request):
 
     context = {'form': form}
     return render(request, 'main/form.html', context)
-
 
 
 def home(request):
