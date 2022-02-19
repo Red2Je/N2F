a8563159e5bb68a958c7e9abf4b89b3f7c6c149d
Red2Je <63746333+Red2Je@users.noreply.github.com>
2022-02-19 19:39:24 +0100

    url

--- n2f/urls.py
+++ n2f/urls.py
@@ -41,6 +41,5 @@ urlpatterns = [
     path('Advance/<int:advId>', views.modifyAdvance, name='ModifyAdvance'),
     path('Mileage/<int:milId>', views.modifyMileage, name='ModifyMileage'),
 
-    path('Refund/<int:refId>/', views.modifyRefund, name='ModifyRefund'),
     path('ConsultRefund/<int:refId>/', views.consultRefund, name='Consult'),
 ]
